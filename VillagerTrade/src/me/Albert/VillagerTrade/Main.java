598d9674f92e096176dcb4a300aa7ab87103a105
mcdoeswhat <2929356483@qq.com>
2019-05-21 21:38:01 +0800

    Add files via upload

--- VillagerTrade/src/me/Albert/VillagerTrade/Main.java
+++ VillagerTrade/src/me/Albert/VillagerTrade/Main.java
@@ -7,11 +7,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.event.entity.VillagerReplenishTradeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -41,43 +42,54 @@ public class Main extends JavaPlugin implements Listener{
 	private boolean cd = true;
 	
 	@EventHandler
-    public void onTrade(VillagerAcquireTradeEvent e) {
-     	int a = this.config.getInt("chance");
+    public void onTrade(VillagerReplenishTradeEvent e) {
+		int a = this.config.getInt("chance");
     	int b=(int)(Math.random()*100);
-    	if (a>=b && e.getEntity().isDead() == false && cd == true && e.getEntity().getTicksLived() > config.getInt("ticks")) {
-        	cd = false;
-   	        e.getEntity().remove();
-   	        if (config.getString("action").equalsIgnoreCase("change")) {
-   	        e.getEntity().getWorld().spawnEntity(e.getEntity().getLocation(),EntityType.VILLAGER);
-   	        }
-   	     if (this.config.getBoolean("consolelogs") == true) {
-   	    	 String action = config.getString("action").toString();
-   	    	if (action.equalsIgnoreCase("change")) {
-   	    		action = " changes his proffesion";
-   	    	}
-   	    	if (action.equalsIgnoreCase("remove")) {
-   	    		action = " has been removed";
-   	    	}
-          	Bukkit.getConsoleSender().sendMessage("§aVillager at X: " +  (int)e.getEntity().getLocation().getX() + " Y: "
-          			+  (int)e.getEntity().getLocation().getY() + " Z: "+ (int)e.getEntity().getLocation().getZ()+ action);
-          }
-   	     
-   	  for(Player p: Bukkit.getOnlinePlayers()) {
-   		  if (e.getEntity().getWorld() == p.getWorld()) {
- 		 if (e.getEntity().getLocation().distanceSquared(p.getLocation()) < 20) {
- 			 p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("message")));
- 		 }
-   	  }
-   	  }
-   	     getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-				public void run() {
-					cd =true;
-  		}
-          }, 10L);
+    	if (a>=b && e.getEntity().isDead() == false && cd == true 
+    		&& e.getEntity().getTicksLived() > config.getInt("ticks")) {
+    		if (config.getBoolean("unlockall") == false) {
+        	changeVillager(e.getEntity());
+    		}
+    		if (config.getBoolean("unlockall") == true && e.getEntity().getRecipeCount() >= 4) {
+    			changeVillager(e.getEntity());
+    		}
    	
     	}
+    	
    	 
     }
+	public void changeVillager(Entity v) {
+		    cd = false;
+	        v.remove();
+	        if (config.getString("action").equalsIgnoreCase("change")) {
+	        v.getWorld().spawnEntity(v.getLocation(),EntityType.VILLAGER);
+	        }
+	     if (this.config.getBoolean("consolelogs") == true) {
+	    	 String action = config.getString("action").toString();
+	    	if (action.equalsIgnoreCase("change")) {
+	    		action = " changed his proffesion";
+	    	}
+	    	if (action.equalsIgnoreCase("remove")) {
+	    		action = " has been removed";
+	    	}
+      	Bukkit.getConsoleSender().sendMessage("§aVillager at X: " +  (int)v.getLocation().getX() + " Y: "
+      			+  (int)v.getLocation().getY() + " Z: "+ (int)v.getLocation().getZ()+ action);
+      }
+	     
+	  for(Player p: Bukkit.getOnlinePlayers()) {
+		  if (v.getWorld() == p.getWorld()) {
+		 if (v.getLocation().distanceSquared(p.getLocation()) < 20) {
+			 p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("message")));
+		 }
+	  }
+	  }
+	     getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			public void run() {
+				cd =true;
+		}
+      }, 10L);
+		
+	}
 	
 
 }
