db1e9d048f7c6eaa21a3993d80fc8f73c7eccde7
mcdoeswhat <2929356483@qq.com>
2019-05-21 13:10:34 +0800

    Completely recoded
    Completely recoded using the VillagerAcquireTradeEvent
    and new features

--- VillagerTrade/src/me/Albert/VillagerTrade/Main.java
+++ VillagerTrade/src/me/Albert/VillagerTrade/Main.java
@@ -4,18 +4,14 @@ import java.io.File;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.entity.VillagerAcquireTradeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -32,6 +28,7 @@ public class Main extends JavaPlugin implements Listener{
 		 this.config = YamlConfiguration.loadConfiguration(this.file);
 		 new Metrics(this);	
 	};
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!new File(this.getDataFolder(),"config.yml").exists()) {
 		saveResource("config.yml", true);
@@ -41,43 +38,46 @@ public class Main extends JavaPlugin implements Listener{
 		return true;
 	}
 	
-	Villager v1;
-	World w;
-	Player p;
-	@EventHandler
-    public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event){
-		if (event.getRightClicked().getType() == EntityType.VILLAGER) {
-                 v1=(Villager) event.getRightClicked();
-                w = event.getPlayer().getWorld();
-                p = event.getPlayer();
-		}
-	}
+	private boolean cd = true;
 	
 	@EventHandler
-    public void onVillagerTrade(InventoryClickEvent event) {
-        if ((event.getInventory() != null) && (event.getInventory().getType() == InventoryType.MERCHANT)) {
-        	int a = this.config.getInt("chance");
-        	int b=(int)(Math.random()*100);
-        	if (event.getRawSlot() == 2 && v1.isDead() == false && a>=b) {
-        		if (event.getAction().toString().contains("PICKUP") || event.getAction().toString().contains("MOVE_TO")) {
-        		v1.remove();
-                w.spawnEntity(v1.getLocation(), EntityType.VILLAGER);
-                if (this.config.getBoolean("consolelogs") == true) {
-                	Bukkit.getConsoleSender().sendMessage("§aVillager at X: " +  (int)v1.getLocation().getX() + " Y: "
-                			+  (int)v1.getLocation().getY() + " Z: "+ (int)v1.getLocation().getZ()+ " changed his proffession");
-                }
-                getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-    				public void run() {
-                p.closeInventory();
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("message")));
-        		}
-                }, 10L);
-            
-        	}
-          
-        }
+    public void onTrade(VillagerAcquireTradeEvent e) {
+     	int a = this.config.getInt("chance");
+    	int b=(int)(Math.random()*100);
+    	if (a>=b && e.getEntity().isDead() == false && cd == true && e.getEntity().getTicksLived() > config.getInt("ticks")) {
+        	cd = false;
+   	        e.getEntity().remove();
+   	        if (config.getString("action").equalsIgnoreCase("change")) {
+   	        e.getEntity().getWorld().spawnEntity(e.getEntity().getLocation(),EntityType.VILLAGER);
+   	        }
+   	     if (this.config.getBoolean("consolelogs") == true) {
+   	    	 String action = config.getString("action").toString();
+   	    	if (action.equalsIgnoreCase("change")) {
+   	    		action = " changes his proffesion";
+   	    	}
+   	    	if (action.equalsIgnoreCase("remove")) {
+   	    		action = " has been removed";
+   	    	}
+          	Bukkit.getConsoleSender().sendMessage("§aVillager at X: " +  (int)e.getEntity().getLocation().getX() + " Y: "
+          			+  (int)e.getEntity().getLocation().getY() + " Z: "+ (int)e.getEntity().getLocation().getZ()+ action);
+          }
+   	     
+   	  for(Player p: Bukkit.getOnlinePlayers()) {
+   		  if (e.getEntity().getWorld() == p.getWorld()) {
+ 		 if (e.getEntity().getLocation().distanceSquared(p.getLocation()) < 20) {
+ 			 p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("message")));
+ 		 }
+   	  }
+   	  }
+   	     getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				public void run() {
+					cd =true;
+  		}
+          }, 10L);
+   	
+    	}
+   	 
     }
 	
 
-}
 }
